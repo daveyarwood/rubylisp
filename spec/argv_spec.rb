763d562b4571ec862e96d5339f94e4eef1b92757
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-04 20:19:24 -0500

    remove failing ARGV test
    argv is not not reliably [], depending on how you run the tests

--- spec/argv_spec.rb
+++ spec/argv_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "ARGV" do
   with_rubylisp_env do
     expect_outputs \
-      'true', '(= Kernel::Array (class Kernel::ARGV))',
-      '[]', 'Kernel::ARGV'
+      'true', '(= Kernel::Array (class Kernel::ARGV))'
   end
 end
